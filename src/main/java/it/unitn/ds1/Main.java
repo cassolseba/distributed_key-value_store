2611b2155612f88c6250fce5fd598314e6d9d5f5
Sebastiano Cassol <cassol.sebastiano@gmail.com>
2023-09-01 23:08:46 +0200

    Added read tests and some update tests in Main

--- src/main/java/it/unitn/ds1/Main.java
+++ src/main/java/it/unitn/ds1/Main.java
@@ -24,6 +24,73 @@ public class Main {
     private static final int dataCount = 10;
     private static final int clientCount = 3;
 
+    public static void main(String[] args) throws InterruptedException {
+        /* Instantiate a new distributed key-value store */
+        DistributedKeyValueStore database = new DistributedKeyValueStore(
+                "DKVSystem", N, W, R, dataNodeCount, clientCount);
+
+        /* Get references to the clients */
+        ActorRef firstClient = database.getClient(0);
+        ActorRef secondClient = database.getClient(1);
+        ActorRef thirdClient = database.getClient(2);
+
+        /* Get references to the data nodes */
+        ActorRef firstDataNode = database.getDataNode(0); // key: 10
+        ActorRef secondDataNode = database.getDataNode(1); // key: 20
+        ActorRef thirdDataNode = database.getDataNode(2); // key: 30
+        ActorRef fourthDataNode = database.getDataNode(3); // key: 40
+        ActorRef fifthDataNode = database.getDataNode(4); // key: 50
+
+        /* Wait for the system to be ready */
+        Thread.sleep(2500);
+
+        /* Insert some data in the distributed database */
+        database.sendWriteFromClient(firstClient, firstDataNode, 5, "FIVE");
+        database.sendWriteFromClient(firstClient, firstDataNode, 17, "SEVENTEEN");
+        database.sendWriteFromClient(firstClient, firstDataNode, 29, "TWENTY-NINE");
+        database.sendWriteFromClient(firstClient, firstDataNode, 43, "FORTY-THREE");
+
+        /* Wait for the writes to be completed */
+        Thread.sleep(2500);
+
+        /* ----- TESTS ----- */
+
+        // test 1: read from a key
+        // TODO fix READ_REPLY names, it seems duplicated but it is not
+//        database.sendReadFromClient(firstClient, firstDataNode, 17);
+
+        // test 2: read form an unknown key
+        // TODO handle read from unknown key
+//        database.sendReadFromClient(firstClient, firstDataNode, 21);
+
+        // test 3: update a value in the database
+//        database.sendUpdateFromClient(firstClient, firstDataNode, 17, "SE-VEN-TEEN");
+
+        // test 4: update an unknown key
+        // TODO handle update from unknown key
+//        database.sendUpdateFromClient(firstClient, firstDataNode, 21, "TWENTY-ONE");
+
+        // test 5: read a key twice from the same client and the same coordinator
+//        database.sendReadFromClient(firstClient, firstDataNode, 17);
+//        database.sendReadFromClient(firstClient, firstDataNode, 17);
+
+        // test 6: read a key from two different clients and two different coordinators
+//        database.sendReadFromClient(firstClient, firstDataNode, 17);
+//        database.sendReadFromClient(secondClient, secondDataNode, 17);
+
+        // test 7: read a key from two different clients and the same coordinator
+//        database.sendReadFromClient(firstClient, firstDataNode, 17);
+//        database.sendReadFromClient(secondClient, firstDataNode, 17);
+
+
+
+
+
+        Thread.sleep(10000);
+        System.exit(0);
+    }
+
+    /*
     public static void main(String[] args) {
         DistributedKeyValueStore database = new DistributedKeyValueStore(
                 "DKVSystem", N, W, R, dataNodeCount, clientCount);
@@ -107,8 +174,6 @@ public class Main {
         database.recover(crasher, bootstrapper);
 
         inputContinue();
-
-        System.exit(0);
     }
 
     public static void inputContinue() {
@@ -116,5 +181,5 @@ public class Main {
             System.out.println("-- ENTER to continue --");
             System.in.read();
         } catch (IOException ignored) {}
-    }
+    } */
 }
